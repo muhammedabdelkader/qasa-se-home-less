b4d4e8a0d48e746bab8472c76f9b07a1ef01f66f
Muhammed Abdelkader <muhammed.m.abdelkader@gmail.com>
2021-10-27 20:40:15 -0400

    TODO added

--- qasa.py
+++ qasa.py
@@ -16,11 +16,26 @@ class find_home_in_qasa:
     {"operationName":"CreateHomeApplication","variables":{"home_id":"323122","counter_offer":18500,"message":"dsds"},"query":"mutation CreateHomeApplication($home_id: ID!, $counter_offer: Int!, $start_optimal: DateTime, $end_optimal: DateTime, $message: String) {\n  createTenantHomeApplication(\n    input: {homeId: $home_id, counterOffer: $counter_offer, startOptimal: $start_optimal, endOptimal: $end_optimal, message: $message}\n  ) {\n    homeApplication {\n      message\n      endOptimal\n      endUfn\n      id\n      startOptimal\n      status\n      createdAt\n      currency\n      matchInterest\n      origin\n      tenant {\n        uid\n        landlord\n        tenant\n        __typename\n      }\n      home {\n        id\n        __typename\n      }\n      __typename\n    }\n    errors {\n      field\n      codes\n      __typename\n    }\n    __typename\n  }\n}\n"}
     """
     def postMessage(self, home_id, counter_offer, message):
+        """
+            @TODO 
+            Shall be called out by Link /sendMessageTo = home_id 
+            counter_offer is same cost 
+            message body is the text we keep it 
+        """
         query = {"operationName":"CreateHomeApplication","variables":{"home_id":f"{home_id}","counter_offer":counter_offer,"message":f"{message}"},"query":"mutation CreateHomeApplication($home_id: ID!, $counter_offer: Int!, $start_optimal: DateTime, $end_optimal: DateTime, $message: String) {  createTenantHomeApplication(    input: {homeId: $home_id, counterOffer: $counter_offer, startOptimal: $start_optimal, endOptimal: $end_optimal, message: $message}  ) {    homeApplication {      message      endOptimal      endUfn      id      startOptimal      status      createdAt      currency      matchInterest      origin      tenant {        uid        landlord        tenant        __typename      }      home {        id        __typename      }      __typename    }    errors {      field      codes      __typename    }  __typename  }}"}
 
         response = requests.post(url=self.base_url, headers=self.headers, json=query)
         error = response.json()['data']['createTenantHomeApplication']['error']
         return home_id,error
+
+    def send_email(self,message):
+        """
+        @TODO 
+        Send email with 
+            the link to view 
+            the  hyperlink to post the person message 
+        """
+        print(send me message)
 
 
     def find_my_home(self,region='se/stockholms_län'):
